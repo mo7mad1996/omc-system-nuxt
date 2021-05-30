2d2ef905718352702ef9108e156272ab07dcc653
Mohamed Ibrahem <mo7mad369@gmail.com>
2021-05-30 09:55:50 +0200

    finsh frist update for project

--- layouts/error.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>404 error not found</section>
-</template>
-
-<script>
-export default {
-  layout: 'basic',
-}
-</script>
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 start json-server --host 192.168.0.2 db.json
 start npm run dev
 start http://192.168.0.2:1996/
-code .
+start code .
