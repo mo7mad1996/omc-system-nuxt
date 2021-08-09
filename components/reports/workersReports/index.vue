b818eb44d2b3f9982b82be4fa481ed1959dcf3de
Mohamed Ibrahem <mo7mad369@gmail.com>
2021-08-09 15:03:00 +0200

    start in xslx files

--- components/reports/workersReports/index.vue
+++ components/reports/workersReports/index.vue
@@ -173,7 +173,6 @@ export default {
   },
 
   components: { EditForm },
-  mounted() {},
 }
 </script>
 
---
+++ pages/t/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><input type="file" ref="file" @change="change" /></div>
+</template>
+
+<script>
+export default {
+  data: () => ({ file: '' }),
+  methods: {
+    change() {
+      this.file = this.$refs.file.files[0]
+      let fr = new FileReader()
+      fr.onload = () => {
+        console.log(fr)
+      }
+
+      fr.readAsDataURL(this.file)
+    },
+  },
+}
+</script>
